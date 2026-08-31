a15650948ce009c3a072422ebc0cba414ba50700
rajaryansharma56-dot <rajaryansharma56@gmail.com>
2026-08-31 23:24:17 +0530

    Implement pointer manipulation in C program

---
+++ pointer.cpp
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h> // Required for the abs() function
+
+void update(int *a, int *b) {
+    // Store original values before modification
+    int original_a = *a;
+    
+    // Update 'a' to be the sum of both numbers
+    *a = original_a + *b;
+    
+    // Update 'b' to be the absolute difference between both numbers
+    *b = abs(original_a - *b);
+}
+
+int main() {
+    int a, b;
+    int *pa = &a, *pb = &b;
+    
+    scanf("%d %d", &a, &b);
+    update(pa, pb);
+    printf("%d\n%d", a, b);
+
+    return 0;
+}
