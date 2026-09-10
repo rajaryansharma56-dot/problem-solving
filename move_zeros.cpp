69aa179f266d47b940b38a3a889580cbe4aba141
Raj Aryan Sharma <rajaryansharma56@gmail.com>
2026-09-10 23:19:36 +0530

    Implement move zeros algorithm in C++
    This program moves all zeros in an array to the end while maintaining the order of non-zero elements.

---
+++ move_zeros.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int j = 0;
+
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            arr[j] = arr[i];
+            j++;
+        }
+    }
+
+    while (j < n) {
+        arr[j] = 0;
+        j++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
