01d148c0ae7ad6c906e821b2226e50af25910cd9
Raj Aryan Sharma <rajaryansharma56@gmail.com>
2026-09-15 19:25:30 +0530

    Add program to count the digits of a number

---
+++ count_digits.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter a number :"<<endl;
+    cin>>n;
+
+    int count=0;
+
+    while(n>0){
+        n=n/10;
+        count++;
+
+    }
+
+    cout<<"number of digits = "<<count<<endl;
+
+return 0;
+
+}
