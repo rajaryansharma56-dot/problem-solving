bf5478a3ea2105a9db7b23106ba2fe6811d622c0
Raj Aryan Sharma <rajaryansharma56@gmail.com>
2026-09-22 22:16:49 +0530

    Add multiplication table program in C++

---
+++ multiplication_table.cpp
@@ -0,0 +1,18 @@
+
+int main(){
+    int n;
+    cout<<"enter number :";
+    cin>>n;
+
+    int product=1;
+
+
+    for(int i=1; i<=10; i++){
+       cout<<n<<"x"<<i<<" ="<<n*i<<endl;
+       
+
+    }
+
+    return 0;
+
+}
